e766fd2a09f820f6d0b8d278998fb48f5906bf4c
Margot Cooper Gruen <margotgruen@gmail.com>
2022-11-30 21:41:24 -0800

    Added user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,25 @@
-//placeholder for now
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+const userCount = async () => {
+  User.aggregate()
+    .count("userCount")
+    .then((numberOfUsers) => numberOfUsers);
+};
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObject = {
+          users,
+          userCount: await userCount(),
+        };
+        return res.json(userObject);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
